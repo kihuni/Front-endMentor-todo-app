02b1c38454b2397e2c33a35c3106faf2578fac86
Stephen kihuni <stephenkihuni55@gmail.com>
2022-05-27 17:21:11 +0300

    added some functionality

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,34 @@
-
 import React, { useState } from "react";
-import './Header.css'
+import sunIcon from "../images/icon-sun.svg";
+import moonIcon from "../images/icon-moon.svg";
+import "./Header.css";
 
 const Header = () => {
+  const [theme, setTheme] = useState("dark");
+  const [iconTheme, setIconTheme] = useState(sunIcon);
+
+  const changeTheme = () => {
+    setTheme(theme == "dark" ? "light" : "dark");
+    setIconTheme(theme == "dark" ? moonIcon : sunIcon);
+  };
   return (
-<div>
-    <div className='header'></div>
-    <div className='background'></div>
+    <div>
+      <div className="header"></div>
+      <div className="background"></div>
 
-    <div className="todo">
-        <div className="title">
-            <h1>T O D O</h1>
-            
+      <div className="header-box">
+        <div className="header-sbox">
+          <h1 className="title">T O D O</h1>
+          <img
+            className="icons"
+            src={iconTheme}
+            alt="img icons"
+            onClick={changeTheme}
+          />
         </div>
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
